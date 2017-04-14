c132c81f639ae29fa5007627aa7957bc862f47cd
Victor <vxiaohan@outlook.com>
2017-04-14 10:22:08 -0700

    Save in Mysql

--- smzdm/pipelines.py
+++ smzdm/pipelines.py
@@ -4,8 +4,39 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import MySQLdb
+import logging
 
 
 class SmzdmPipeline(object):
+    def __init__(self):
+        self.conn = MySQLdb.connect(host='localhost', user='root', passwd='mysql900127', db='test_python', charset="utf8")
+
     def process_item(self, item, spider):
+        sql_select = "SELECT id FROM smzdm WHERE article_id =  '%s'" % item['article_id']
+        try:
+            self.cursor.execute(sql_select)
+            result = self.cursor.fetchall()
+            if(len(result) == 0):
+                sql_insert = "INSERT INTO smzdm (article_id,article_url,title,price,source,submit_time)" + \
+                        " VALUES ('%s','%s','%s','%s','%s',from_unixtime(%d))" % \
+                        (item['article_id'], item['article_url'], item['title'], item['price'], item['source'], int(item['time']))
+                self.cursor.execute(sql_insert)
+                self.conn.commit()
+                # print 'Insert article: %s' % item['article_id']
+            else:
+                # print 'Repeat article'
+                pass
+        except:
+            logging.error(str(item))
+            print "Error: unable to fecth data"
         return item
+
+    def open_spider(self, spider):
+        self.cursor = self.conn.cursor()
+        print '++++++++++++++++++++++++++++++++++++++++++++'
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.conn.close()
+        print '----------------------------------------------'
--- smzdm/spiders/cheap_thing.py
+++ smzdm/spiders/cheap_thing.py
@@ -6,30 +6,24 @@ class CheapThings(scrapy.Spider):
     name = 'CheapThings'
     base_url_string = 'http://faxian.smzdm.com/9kuai9/p'
     start_urls = []
-    for i in range(1, 3):
+    for i in range(1, 100):
        start_urls.append(base_url_string+str(i)+'/')
 
     def parse(self, response):
-        if response.url.split('/')[-2] == '9kuai9':
-            filename = 'result/p1.html'
-        else:
-            filename = 'result/' + response.url.split('/')[-2]+'.html'
-        print(filename)
-        print(response.url.split('/'))
         items = []
-        for sel in response.xpath('//*[@id="feed-main-list"]/li'):
-            print sel.xpath('div[@class="feed-block-ver "]/div[@class="feed-ver-pic"]/a[@href and not(@class)]').xpath('@href').extract()
+        article_list = response.xpath('//*[@id="feed-main-list"]/li')
+        print response.url
+        for sel in article_list:
             item = CheapThingsListItem()
-            item['time'] = sel.xpath('@timesort').extract()
-            item['article_id'] = sel.xpath('@articleid').extract()
-            item['article_url'] = sel.xpath('div[@class="feed-block-ver "]/div[@class="feed-ver-pic"]/a[@href and not(@class)]').xpath('@href').extract()
-            item['title'] = sel.xpath('div[@class="feed-block-ver "]/h5/a/text()').extract()
-            item['price'] = sel.xpath('div[@class="feed-block-ver "]/div[@class="z-highlight z-ellipsis"]/text()').extract()
-            item['source'] = sel.xpath('div[@class="feed-block-ver "]/div[@class="feed-ver-pic"]/a[@class]/text()').extract()
-            print(item)
+            item['time'] = sel.xpath('@timesort').extract()[0]
+            item['article_id'] = sel.xpath('@articleid').extract()[0]
+            item['article_url'] = sel.xpath('div[@class="feed-block-ver "]/div[@class="feed-ver-pic"]/a[@href and not(@class)]').xpath('@href').extract()[0]
+            item['title'] = sel.xpath('div[@class="feed-block-ver "]/h5/a/text()').extract()[0]
+            item['price'] = sel.xpath('div[@class="feed-block-ver "]/div[@class="z-highlight z-ellipsis"]/text()').extract()[0]
+            item['source'] = sel.xpath('div[@class="feed-block-ver "]/div[@class="feed-ver-pic"]/a[@class]/text()').extract()[0]
             items.append(item)
-            print(type(item['article_url']))
-            yield scrapy.Request(item['article_url'][0], callback=self.get_article_detail)
+            # yield scrapy.Request(item['article_url'][0], callback=self.get_article_detail)
+        return items
 
     def get_article_detail(self, response):
         print(response.url)
